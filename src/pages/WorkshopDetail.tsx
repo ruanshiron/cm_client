2932ed8f18c2098246466de2cc3961e5af19d68d
ruanshiron <sandt26@gmail.com>
2020-12-22 09:59:32 +0700

    detail mock

--- src/pages/WorkshopDetail.tsx
+++ src/pages/WorkshopDetail.tsx
@@ -1,62 +1,12 @@
-import { ActionSheetButton, IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonToolbar } from "@ionic/react";
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonToolbar } from "@ionic/react";
 import { callOutline, callSharp, shareOutline, shareSharp } from "ionicons/icons";
-import React, { useState } from "react";
+import React from "react";
 
 import "./WorkshopDetail.scss";
 
 interface WorkshopDetailProps {}
 
 export const WorkshopDetail: React.FC<WorkshopDetailProps> = () => {
-  const [showActionSheet, setShowActionSheet] = useState(false);
-  const [actionSheetButtons, setActionSheetButtons] = useState<
-    ActionSheetButton[]
-  >([]);
-  const [actionSheetHeader, setActionSheetHeader] = useState("");
-
-  function openWorkshopShare(workshop: any) {
-    setActionSheetButtons([
-      {
-        text: "Copy Link",
-        handler: () => {
-          console.log("Copy Link clicked");
-        },
-      },
-      {
-        text: "Share via ...",
-        handler: () => {
-          console.log("Share via clicked");
-        },
-      },
-      {
-        text: "Cancel",
-        role: "cancel",
-        handler: () => {
-          console.log("Cancel clicked");
-        },
-      },
-    ]);
-    setActionSheetHeader(`Share ${workshop.name}`);
-    setShowActionSheet(true);
-  }
-
-  function openContact(workshop: any) {
-    setActionSheetButtons([
-      {
-        text: `Email ( ${workshop.email} )`,
-        handler: () => {
-          window.open("mailto:" + workshop.email);
-        },
-      },
-      {
-        text: `Call ( ${workshop.phone} )`,
-        handler: () => {
-          window.open("tel:" + workshop.phone);
-        },
-      },
-    ]);
-    setActionSheetHeader(`Share ${workshop.name}`);
-    setShowActionSheet(true);
-  }
 
   return (
     <>
@@ -68,14 +18,14 @@ export const WorkshopDetail: React.FC<WorkshopDetailProps> = () => {
                 <IonBackButton defaultHref="/tabs/account" />
               </IonButtons>
               <IonButtons slot="end">
-                <IonButton onClick={() => openContact({})}>
+                <IonButton>
                   <IonIcon
                     slot="icon-only"
                     ios={callOutline}
                     md={callSharp}
                   ></IonIcon>
                 </IonButton>
-                <IonButton onClick={() => openWorkshopShare({})}>
+                <IonButton>
                   <IonIcon
                     slot="icon-only"
                     ios={shareOutline}
