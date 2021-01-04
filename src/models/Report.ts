9074878009b141d5ee8d87d7c2f952fd4b8a8952
ruanshiron <sandt26@gmail.com>
2021-01-04 23:43:28 +0700

    report

--- src/models/Report.ts
+++ src/models/Report.ts
@@ -4,4 +4,10 @@ export interface Product {
   name?: string;
   sizes?: string[];
   note?: string;
+  reports?: Field[];
+}
+
+export interface Field {
+  name: string;
+  value: any;
 }
--- src/pages/ReportDetail.tsx
+++ src/pages/ReportDetail.tsx
@@ -5,15 +5,50 @@ import {
   IonContent,
   IonHeader,
   IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote,
   IonPage,
   IonToolbar,
 } from "@ionic/react";
 import { shareOutline, shareSharp } from "ionicons/icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useSelector } from "../store";
+import _ from "lodash";
+import { Field } from "../models/Report";
 
 interface ReportDetailProps {}
 
 export const ReportDetail: React.FC<ReportDetailProps> = () => {
+  const { id } = useParams<{ id: string }>();
+  const [fields, setFields] = useState<Field[]>();
+
+  const report = useSelector((state) =>
+    state.report.products.find((x) => x.id === id)
+  );
+
+  const events = useSelector((state) =>
+    state.diary.events.filter((x) => x.productCode === id)
+  );
+
+  useEffect(() => {
+    if (!report) return;
+
+    const result = _.groupBy(events, "typeCode");
+
+    const newFields = Object.keys(result).map((key) => {
+      return {
+        name: key,
+        value: result[key].reduce((a, b) => a + (b ? b?.quantity! : 0), 0),
+      };
+    });
+
+    setFields(newFields);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <IonPage>
@@ -33,7 +68,20 @@ export const ReportDetail: React.FC<ReportDetailProps> = () => {
             </IonButtons>
           </IonToolbar>
         </IonHeader>
-        <IonContent></IonContent>
+        <IonContent>
+          <IonList lines="none">
+            {fields?.map((field: any) => (
+              <IonItem detail={false} key={field.name}>
+                <IonLabel>
+                  <h3>{field.name}</h3>
+                </IonLabel>
+                <IonNote slot="end">
+                  <h3>{field.value}</h3>
+                </IonNote>
+              </IonItem>
+            ))}
+          </IonList>
+        </IonContent>
       </IonPage>
     </>
   );
