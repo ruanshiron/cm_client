17b72ed6580c854df4ba8abf50d199e71387b3e0
ruanshiron <sandt26@gmail.com>
2021-04-08 15:18:22 +0700

    customer aggregate

--- src/pages/lists/CustomerDetail.tsx
+++ src/pages/lists/CustomerDetail.tsx
@@ -4,6 +4,8 @@ import {
   IonButtons,
   IonCard,
   IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
   IonCol,
   IonContent,
   IonGrid,
@@ -12,6 +14,7 @@ import {
   IonItem,
   IonLabel,
   IonList,
+  IonNote,
   IonPage,
   IonRow,
   IonTitle,
@@ -25,6 +28,7 @@ import {
   shareOutline,
   shareSharp,
 } from "ionicons/icons";
+import { chain, sum } from "lodash";
 import React from "react";
 import { useParams } from "react-router";
 import { useSelector } from "../../store";
@@ -38,6 +42,19 @@ export const CustomerDetail: React.FC<CustomerDetailProps> = () => {
   const customer = useSelector((state) =>
     state.data.customers.find((v) => v.id === id)
   );
+
+  const data = useSelector((state) => {
+    const orders = state.data.orders
+      .filter((v) => v.customer === id)
+      .flatMap((v) => v.lines);
+    return chain(orders)
+      .groupBy("product")
+      .map((value, key) => ({
+        name: state.data.products.find((v) => v.id === key)?.name,
+        aggregate: sum(value.map((v) => v.quantity)),
+      }))
+      .value();
+  });
   return (
     <>
       <IonPage id="customer-detail">
@@ -77,9 +94,31 @@ export const CustomerDetail: React.FC<CustomerDetailProps> = () => {
                         <IonLabel slot="start">{customer?.name}</IonLabel>
                       </IonItem>
                       <IonItem>
-                        <IonIcon icon={phonePortraitOutline} slot="start"></IonIcon>
-                        <IonLabel slot="start">{customer?.phonenumber}</IonLabel>
+                        <IonIcon
+                          icon={phonePortraitOutline}
+                          slot="start"
+                        ></IonIcon>
+                        <IonLabel slot="start">
+                          {customer?.phonenumber}
+                        </IonLabel>
                       </IonItem>
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardSubtitle>Sản phẩm đã đặt hàng</IonCardSubtitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <IonList lines="full">
+                      {data.map((e) => (
+                        <IonItem>
+                          <IonLabel slot="start">{e.name}</IonLabel>
+                          <IonNote slot="end">
+                            <p>{e.aggregate}</p>
+                          </IonNote>
+                        </IonItem>
+                      ))}
                     </IonList>
                   </IonCardContent>
                 </IonCard>
